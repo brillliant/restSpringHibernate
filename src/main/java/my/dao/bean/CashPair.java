d51b933a0bb5be6ba7e535686f1c3fb3eaf663bf
brillliant@tut.by <5766669Z>
2017-12-27 16:27:13 +0300

    не работает. база отключена.  раньше без хибера работало.

--- src/main/java/my/dao/bean/CashPair.java
+++ src/main/java/my/dao/bean/CashPair.java
@@ -1,24 +1,12 @@
 package my.dao.bean;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import java.io.Serializable;
 
-@Entity
-@Table(name = "cash_pair")
 public class CashPair implements Serializable {
 
-    @Id
-    @Column(name = "key")
     private String key;
 
-    @Column(name = "value")
     private String value;
-
-    public CashPair() {
-    }
 
     public CashPair(String key, String value) {
         this.key = key;
--- src/main/java/my/service/CashService.java
+++ src/main/java/my/service/CashService.java
@@ -1,6 +1,5 @@
 package my.service;
 
-import my.dao.CashPairDAO;
 import my.dao.bean.CashPair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,31 +9,20 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
-@Transactional
 public class CashService {
 
-    @Autowired
-    CashPairDAO cashPairDAO;
-
     private Map<String,String> concurrentHashMap = new ConcurrentHashMap();
-
 
     public CashPair setCash(String key, String value){
         concurrentHashMap.put(key, value);
         CashPair cashPair = new CashPair(key, value);
-        //cashPairDAO.save(cashPair);
         return cashPair;
     }
 
     public CashPair getCash(String key){
         CashPair cashPair;
         String value = concurrentHashMap.get(key);
-
-        if(value != null){
-            cashPair = new CashPair(key, value);
-        } else {
-            cashPair = cashPairDAO.getCashPair(key);
-        }
+        cashPair = new CashPair(key, value);
         return cashPair;
     }
 }
